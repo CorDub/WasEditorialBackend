b4e2e3d7a4222c74c418aae10f9756e915a873f5
Corentin Dubois <corentindubois22@gmail.com>
2025-01-27 15:22:59 -0600

    added backend (node / express / prisma / postgreSQL) and frontend (Vite / React) configs

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Example from './Example.jsx'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <Example />
+      </div>
+
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/Example.jsx
@@ -0,0 +1,47 @@
+import { useState, useEffect } from 'react';
+
+function Example () {
+  const [message, setMessage] = useState('');
+  const [users, setUsers] = useState([]);
+
+  useEffect(()=>{
+    fetch('api/message')
+      .then((response) => response.json())
+      .then((data) => setMessage(data.message))
+  }, []);
+
+  async function fetchUsers() {
+    try {
+      const response = await fetch('api/users');
+      const data = await response.json();
+      console.log(data);
+      setUsers(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  console.log(users);
+
+  return (
+    <>
+      <div>
+        <p>Message from backend: {message}</p>
+      </div>
+      <div>
+        <p>Message from the database:</p>
+        <ul>
+          {users && users.map((user) => {
+            return <li key={user.id}>{user.name} -  {user.email}</li>
+          })}
+        </ul>
+      </div>
+    </>
+  )
+}
+
+export default Example;

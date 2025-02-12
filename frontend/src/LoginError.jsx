e2607c4ac36af32c81106fb9c184048757e80237
Corentin Dubois <corentindubois22@gmail.com>
2025-02-11 19:40:36 -0600

    Added a check for other common errors in the login form: empty fields, values too long, wrong type passed.

---
+++ frontend/src/LoginError.jsx
@@ -0,0 +1,25 @@
+import useCheckUser from "./useCheckUser";
+
+function LoginError({ errors }) {
+  useCheckUser();
+  console.log(errors);
+
+  const errorStrings = [
+    "El correo o la contraseña no son válidos",
+    "El correo no puede estar vacio",
+    "La contraseña no puede estar vacia",
+  ]
+
+  return(
+    <>
+      {errors.length === 0 ?
+        null :
+        errors.map((error, index) => (
+          <p key={index}>{errorStrings[index]}</p>
+        ))
+      }
+    </>
+  )
+};
+
+export default LoginError;

085f89fc40a6ac6f51bc64411c78de43cd43b08e
Corentin Dubois <corentindubois22@gmail.com>
2025-02-19 12:46:29 -0600

    Added alerts to DeleteCategoryModal.

--- frontend/src/DeleteCategoryModal.jsx
+++ frontend/src/DeleteCategoryModal.jsx
@@ -1,6 +1,6 @@
 import useCheckUser from "./useCheckUser";
 
-function DeleteCategoryModal( {row, closeDeleteModal }) {
+function DeleteCategoryModal( {row, closeDeleteModal, pageIndex, globalFilter }) {
   useCheckUser();
 
   async function deleteCategory() {
@@ -14,7 +14,11 @@ function DeleteCategoryModal( {row, closeDeleteModal }) {
       });
 
       if (response.ok) {
-        closeDeleteModal;
+        const alertMessage = `La categoría ${row.type} ha sido eliminada con exito.`
+        closeDeleteModal(pageIndex, globalFilter, true, alertMessage, "confirmation");
+      } else {
+        const alertMessage = `No se pudó eliminar la categoría ${row.type}`;
+        closeDeleteModal(pageIndex, globalFilter, false, alertMessage, "error");
       }
 
     } catch (error) {
@@ -23,14 +27,14 @@ function DeleteCategoryModal( {row, closeDeleteModal }) {
   }
 
   return (
-    <div className="modal-overlay" onClick={closeDeleteModal}>
+    <div className="modal-overlay" onClick={() => closeDeleteModal(pageIndex, globalFilter, false)}>
       <div className="modal-proper">
         <div className="delmod-confirm">
           <p>{`¿Está seguro que quiere eliminar la categoria ${row.type}?`}</p>
         </div>
         <div className="modal-actions">
           <button className='blue-button modal-button'
-            onClick={closeDeleteModal}>Cancelar</button>
+            onClick={() => closeDeleteModal(pageIndex, globalFilter, false)}>Cancelar</button>
           <button className='blue-button modal-button'
             onClick={deleteCategory}>Confirmar</button>
         </div>

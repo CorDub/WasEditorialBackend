a76518d6149aa9a64d8bd13ed66c01b83fb4c380
Corentin Dubois <corentindubois22@gmail.com>
2025-03-04 14:52:10 -0600

    Modified DB schema to add a quantity column in the sales table. Default 1.

--- frontend/src/SalesList.jsx
+++ frontend/src/SalesList.jsx
@@ -14,15 +14,150 @@ function SalesList () {
   const [isModalOpen, setModalOpen] = useState(false);
   const [clickedRow, setClickedRow] = useState(null);
   const [modalType, setModalType] = useState("");
+  const [modalAction, setModalAction] = useState("");
   const [forceRender, setForceRender] = useState(false);
   const [alertMessage, setAlertMessage] = useState("");
   const [alertType, setAlertType] = useState("");
-  
+
+
+  const columns = useMemo(() => [
+    {
+      header: "Acciones",
+      Cell: ({row}) => (
+        <div>
+          <button onClick={() => openModal("edit", row.original)}
+            className="blue-button modal-button">Editar</button>
+          <button onClick={() => openModal("delete", row.original)}
+            className="blue-button modal-button">Eliminar</button>
+        </div>
+      )
+    },
+    {
+      header: "Libro",
+      accessorKey:'book.title'
+    },
+    {
+      header: "Libreria",
+      accessorKey: "bookstore.name"
+    },
+    {
+      header: "País",
+      accessorKey: "country"
+    },
+    {
+      header: "Cantidad inicial",
+      accessorKey: "initial"
+    }
+  ], []);
+  const table = useMaterialReactTable({
+    columns,
+    data,
+    enableDensityToggle: false,
+    enableFullScreenToggle: false,
+    enableRowVirtualization: true,
+    renderTopToolbarCustomActions: () => (
+      <div className="table-add-button">
+        <button onClick={() => openModal("adding", null)} className="blue-button">Añadir nuevo inventario</button>
+      </div>
+    ),
+    initialState: {
+      density: 'compact',
+    },
+    onGlobalFilterChange: setGlobalFilter,
+    state: { globalFilter },
+    muiTablePaperProps: {
+      elevation: 0,
+      sx: {
+        borderRadius: '15px',
+        backgroundColor: "#fff",
+        position: "fixed",
+        top: "60px",
+        left: "10px",
+        width: "99vw"
+      }
+    },
+    muiTableBodyRowProps: {
+      sx: {
+        backgroundColor: "#fff",
+      }
+    },
+    muiTableHeadCellProps: {
+      sx: {
+        backgroundColor: "#fff"
+      }
+    },
+    muiTopToolbarProps: {
+      sx: {
+        backgroundColor: "#fff"
+      }
+    },
+    muiBottomToolbarProps: {
+      sx: {
+        backgroundColor: "#fff"
+      }
+    }
+  });
+
+  function openModal(type, clickedRow) {
+    setClickedRow(clickedRow);
+    switch (type) {
+      case 'adding':
+        setModalAction("adding");
+        break;
+      case 'edit':
+        setModalAction("edit");
+        break;
+      case 'delete':
+        setModalAction("delete");
+        break;
+      default:
+        console.log("Unknown error")
+        return;
+    }
+    setModalOpen(true);
+  }
+
+  function closeModal(globalFilter, reload, alertMessage, alertType) {
+    setModalOpen(false);
+    globalFilter && setGlobalFilter(globalFilter);
+    if (reload === true) {
+      setForceRender(!forceRender);
+    }
+    if (alertMessage) {
+      setAlertMessage(alertMessage);
+      setAlertType(alertType);
+    }
+  }
+
+  async function fetchSales() {
+    try {
+      const response = await fetch('http://localhost:3000/admin/sales', {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        credentials: 'include'
+      })
+
+      if (response.ok) {
+        const data = await response.json();
+        setData(data);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchSales();
+  }, [forceRender]);
+
   return (
     <div>
       <Navbar subNav={user.role} active={"ventas"}/>
-      {isModalOpen && <Modal modalType={modalType} clickedRow={clickedRow}
-          closeModal={closeModal} globalFilter={globalFilter} />}
+      {isModalOpen && <Modal modalType={modalType} modalAction={modalAction}
+        clickedRow={clickedRow} closeModal={closeModal}
+        globalFilter={globalFilter} />}
       {data && <MaterialReactTable table={table}/>}
       <Alert message={alertMessage} type={alertType}
         setAlertMessage={setAlertMessage} setAlertType={setAlertType}/>

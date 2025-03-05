71cf978bb84323de76d0d8c9342f853e7380fdc2
Corentin Dubois <corentindubois22@gmail.com>
2025-03-05 16:39:07 -0600

    Finished the AddingSaleModal flow. Added the corresponding route in the backend. Updated Modal component to support the AddingSaleModal. Updated SalesList component.

--- frontend/src/Modal.jsx
+++ frontend/src/Modal.jsx
@@ -4,6 +4,7 @@ import DeleteAdminModal from './DeleteAdminModal';
 import AddingInventoryModal from './AddingInventoryModal';
 import EditInventoryModal from './EditInventoryModal';
 import DeleteInventoryModal from './DeleteInventoryModal';
+import AddingSaleModal from './AddingSaleModal';
 
 function Modal({ modalType, modalAction, clickedRow, closeModal, pageIndex, globalFilter }) {
   const potentialModals = {
@@ -16,6 +17,9 @@ function Modal({ modalType, modalAction, clickedRow, closeModal, pageIndex, glob
       adding: AddingInventoryModal,
       edit: EditInventoryModal,
       delete: DeleteInventoryModal,
+    },
+    sale: {
+      adding: AddingSaleModal,
     }
   };
 

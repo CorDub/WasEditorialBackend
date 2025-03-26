d9245dec1f99951812124f08e9c9acf7727fd393
Corentin Dubois <corentindubois22@gmail.com>
2025-03-26 17:28:02 -0600

    Fixed an issue where the table would not reload on deleting.

--- frontend/src/BookstoreInventory.jsx
+++ frontend/src/BookstoreInventory.jsx
@@ -13,7 +13,6 @@ import ProgressBar from "./ProgressBar";
 function BookstoreInventory({selectedBookstore, selectedLogo}) {
   useCheckAdmin();
   const { inventories, fetchInventories } = useContext(InventoriesContext);
-  const [relevantInventories, setRelevantInventories] = useState([]);
   const [data, setData] = useState([]);
   const bookstoreInventoryRef = useRef()
   const [currentTotal, setCurrentTotal] = useState(0);
@@ -140,7 +139,6 @@ function BookstoreInventory({selectedBookstore, selectedLogo}) {
     setCurrentTotal(currentTotal);
     setInitialTotal(initialTotal);
     const sortedRelevantInventories = relevantInventories.sort((a, b) => b.current - a.current);
-    setRelevantInventories(sortedRelevantInventories);
     setData(sortedRelevantInventories);
   }
 
@@ -173,6 +171,7 @@ function BookstoreInventory({selectedBookstore, selectedLogo}) {
     setModalOpen(false);
     globalFilter && setGlobalFilter(globalFilter);
     if (reload === true) {
+      fetchInventories();
       setForceRender(!forceRender);
     }
     if (alertMessage) {
@@ -190,15 +189,6 @@ function BookstoreInventory({selectedBookstore, selectedLogo}) {
         selectedLogo={selectedLogo}
         currentTotal={currentTotal}
         initialTotal={initialTotal}/>
-      {/* {relevantInventories.map((inventory, index) => {
-        return (
-          <BookstoreInventoryBook
-            key={index}
-            title={inventory.book.title}
-            current={inventory.current}
-            initial={inventory.initial}/>
-        )
-      }) */}
       {isModalOpen && <Modal modalType={modalType} modalAction={modalAction} clickedRow={clickedRow}
           closeModal={closeModal} globalFilter={globalFilter} />}
       {data && <MaterialReactTable table={table}/>}

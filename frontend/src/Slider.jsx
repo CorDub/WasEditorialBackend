8e3b66dc9ae2b03d18cc604bcc7a85ca039ea622
Corentin Dubois <corentindubois22@gmail.com>
2025-06-09 14:27:34 -0600

    Made the selection of the size of the letter scalar - as in you choose a value within a scale, you don't just slide around.

--- frontend/src/Slider.jsx
+++ frontend/src/Slider.jsx
@@ -8,15 +8,39 @@ function Slider({value, setNewValue, isEditOpen}) {
   const [isTracking, setTracking] = useState(false);
   const [barRight, setBarRight] = useState(0);
   const [barLeft, setBarLeft] = useState(0);
+  const [gradations, setGradations] = useState({
+    "0.8": 0,
+    "0.9": 0,
+    "1.0": 0,
+    "1.1": 0,
+    "1.2": 0,
+    "1.3": 0,
+    "1.4": 0,
+    "1.5": 0
+  })
 
   useLayoutEffect(() => {
     if (sliderBarRef.current) {
-      setBarRight(sliderBarRef.current.getBoundingClientRect().right)
-      setBarLeft(sliderBarRef.current.getBoundingClientRect().left)
+      const barRight = sliderBarRef.current.getBoundingClientRect().right
+      const barLeft = sliderBarRef.current.getBoundingClientRect().left
+      setBarRight(barRight);
+      setBarLeft(barLeft)
       const percent = (value - 0.8) / 0.7;
-      const length = sliderBarRef.current.getBoundingClientRect().right - sliderBarRef.current.getBoundingClientRect().left
+      const length = barRight - barLeft
       const displacement = length * percent
       setHorizontalDisplacement(displacement);
+
+      const newGradations = {
+        "0.8": 0,
+        "0.9": ((barRight-barLeft) / 7),
+        "1.0": (((barRight-barLeft) / 7) *2),
+        "1.1": (((barRight-barLeft) / 7) *3),
+        "1.2": (((barRight-barLeft) / 7) *4),
+        "1.3": (((barRight-barLeft) / 7) *5),
+        "1.4": (((barRight-barLeft) / 7) *6),
+        "1.5": (((barRight-barLeft) / 7) *7)
+      }
+      setGradations(newGradations);
     }
   }, [value])
 
@@ -31,7 +55,18 @@ function Slider({value, setNewValue, isEditOpen}) {
         diff = barRight - barLeft;
       }
 
-      setHorizontalDisplacement(diff);
+      let closest = null;
+      let smallestDiff = Infinity;
+
+      for (const grade of Object.entries(gradations)) {
+        const distance = Math.abs(diff - grade[1])
+        if (distance < smallestDiff) {
+          smallestDiff = distance;
+          closest = grade[1];
+        }
+      }
+
+      setHorizontalDisplacement(closest);
       const percent = diff / (barRight - barLeft);
       const newValue = (percent * 0.7) + 0.8 ;
       setNewValue(newValue);
@@ -53,22 +88,31 @@ function Slider({value, setNewValue, isEditOpen}) {
 
   }, [isTracking])
 
-  console.log(isEditOpen);
-
   return(
     <div className="slider">
       <div className={isEditOpen ? "slider-bar" : "slider-bar slider-bar-closed"}
         ref={sliderBarRef}>
         {isEditOpen
           ? <div className="slider-ball"
-          ref={sliderBallRef}
-          onMouseDown={() => setTracking(true)}
-          style={{left:`${-4 + horizontalDisplacement}px`}}></div>
+            ref={sliderBallRef}
+            onMouseDown={() => setTracking(true)}
+            style={{left:`${-4 + horizontalDisplacement}px`}}></div>
           : <div className="slider-ball slider-ball-closed"
-          ref={sliderBallRef}
-          style={{left:`${-4 + horizontalDisplacement}px`}}></div>
+            ref={sliderBallRef}
+            style={{left:`${-4 + horizontalDisplacement}px`}}></div>
         }
       </div>
+      {isEditOpen && (
+        <div className="slider-scale">
+          {(Object.entries(gradations)).map((gradation, index) => (
+            <div className="slider-scale-grade"
+              style={{ left: `${gradation[1] - 6}px`}}
+              key={index}>
+              {gradation[0]}
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   )
 }

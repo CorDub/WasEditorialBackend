4d3b1474b388a2870355ac2276f601bf2add1859
Corentin Dubois <corentindubois22@gmail.com>
2025-06-19 12:31:59 -0600

    Better display of costs.

--- frontend/src/TableCosts.jsx
+++ frontend/src/TableCosts.jsx
@@ -1,20 +1,27 @@
 import "./TableCosts.scss";
 import formatNumber from "./customHooks/formatNumber";
 import { useEffect, useState } from "react";
+import TableCostsDetails from "./TableCostsDetails";
 
 function TableCosts({costs}) {
-    const [totalCosts, setTotalCosts] = useState(0)
+    const [totalCosts, setTotalCosts] = useState(0);
+    const [isTotalCostsDetailsOpen, setTotalCostsDetailsOpen] = useState(false);
     
     useEffect(() => {
         let totalCosts = 0;
-        costs.map(cost => totalCosts += cost.amount)
+        costs.map(cost => totalCosts += cost.amount);
         setTotalCosts(totalCosts);
     }, [costs])
 
     return(
-        <div className="table-costs">
-            <div className="table-costs-title">Costos adicionales</div>
-            <div className="table-costs-amount">{formatNumber(totalCosts)}</div>
+        <div className="table-costs"
+            onClick={() => setTotalCostsDetailsOpen(!isTotalCostsDetailsOpen)}>
+            <div className="table-costs-line">
+                <div className="table-costs-title">Costos adicionales</div>
+                <div className="table-costs-amount">- {formatNumber(totalCosts)}</div>
+            </div>
+            {isTotalCostsDetailsOpen && 
+                <TableCostsDetails costs={costs}/>}
         </div>
     )
 }

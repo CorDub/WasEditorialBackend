82a4172376247069b9327e617ee5c06d4e3c026a
Corentin Dubois <corentindubois22@gmail.com>
2025-07-16 16:50:59 -0600

    Completed the new tableBookstores with details.

---
+++ frontend/src/TableBookstoresRowDetails.jsx
@@ -0,0 +1,21 @@
+import formatNumber from "./customHooks/formatNumber";
+import './TableBookstoresRowDetails.scss';
+
+function TableBookstoresRowDetails({monthlySalesData}) {
+  return(
+    <div className="table-row-details">
+      {monthlySalesData && monthlySalesData.bookstores.map((bookstore, index) => (
+        <div key={index} className="table-row-detail">
+          <div className="tbr-first tbr-title tbrd-name">{bookstore.name}</div>
+          <div className="tbr-name">{bookstore.quantity}</div>
+          <div className="tbr-name">{formatNumber(bookstore.price)}</div>
+          <div className="tbr-name">{formatNumber(bookstore.comissions)}</div>
+          <div className="tbr-name">{formatNumber(bookstore.ganancia)}</div>
+          <div className="tbr-name">{formatNumber(bookstore.quantity * bookstore.ganancia)}</div>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default TableBookstoresRowDetails;

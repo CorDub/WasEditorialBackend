6afb51377381e66f193cbfda8dbbfc5c559e87da
Corentin Dubois <corentindubois22@gmail.com>
2026-02-02 17:54:21 -0600

    Fixed an issue where sales and books wwhere not reassigned on bookstore deletion.

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -1053,9 +1053,9 @@ export async function deleteBook(req, res) {
 
       if (deletedBook) {
         const deletedInventoriesIds = await softDeleteInventoriesOnCascade([inputs.id], "books", tx);
-        const deletedKindleSales = await softDeleteKindleSalesOnCascade(deletedBook.id, tx);
+        // const deletedKindleSales = await softDeleteKindleSalesOnCascade(deletedBook.id, tx);
         const deletedCosts = await softDeleteCostsOnCascade(deletedBook.id, tx);
-        await softDeleteSalesOnCascade(deletedInventoriesIds, tx);
+        // await softDeleteSalesOnCascade(deletedInventoriesIds, tx);
         await softDeleteImpressionsOnCascade(deletedBook.id, tx);
       }
 
@@ -1306,6 +1306,74 @@ export async function deleteBookstore(req, res) {
     const prismaClient = req.prisma || prisma
 
     await prismaClient.$transaction(async (tx) => {
+      // Clean out the TBD inventories by sending remaining copies to the WAS inventory
+      const inventoriesToBeDeleted = await tx.inventory.findMany({
+        where: {
+          bookstoreId: inputs.id
+        },
+        include: {
+          sales: true
+        }
+      });
+
+      for (const inventory of inventoriesToBeDeleted) {
+        // Get the Was inventory equivalent
+        let finalInventoryForThatBook;
+        const WasInventoryForThatBook = await tx.inventory.findUnique({
+          where: {
+            bookId_bookstoreId: {
+              bookId: inventory.bookId,
+              bookstoreId: 1
+            }
+          }
+        })
+
+        if (WasInventoryForThatBook && !WasInventoryForThatBook.isDeleted) {finalInventoryForThatBook = WasInventoryForThatBook}
+
+        if (!WasInventoryForThatBook) {
+          const newWasInventoryForThatBook = await tx.inventory.create({
+            data: {
+              bookId: inventory.bookId,
+              bookstoreId: 1
+            }
+          })
+          finalInventoryForThatBook = newWasInventoryForThatBook
+        }
+
+        if (WasInventoryForThatBook && WasInventoryForThatBook.isDeleted) {
+          const deletedWasInventory = await tx.inventory.delete({where: {id: WasInventoryForThatBook.id}})
+          const recreatedWasInventoryForThatBook = await tx.inventory.create({
+            data: {
+              bookId: inventory.bookId,
+              bookstoreId: 1
+            }
+          })
+          finalInventoryForThatBook = recreatedWasInventoryForThatBook;
+        }
+
+        // Create transfer
+        const newTransfer = await tx.transfer.create({
+          data: {
+            fromInventoryId: inventory.id ,
+            toInventoryId: finalInventoryForThatBook.id,
+            quantity: inventory.current,
+            type: "return"
+          }
+        })
+
+        // Move the sales from old inventory to was inventory so they keep appearing
+        for (const sale of inventory.sales) {
+          const updatedSale = await tx.sale.update({
+            where: {
+              id: sale.id
+            },
+            data: {
+              inventoryId: finalInventoryForThatBook.id
+            }
+          })
+        }
+      }
+
       const deletedBookstore = await tx.bookstore.update({where:
         {id: inputs.id},
         data: {
@@ -1315,7 +1383,7 @@ export async function deleteBookstore(req, res) {
 
       if (deletedBookstore) {
         const deletedInventoriesIds = await softDeleteInventoriesOnCascade([inputs.id], "bookstores", tx);
-        await softDeleteSalesOnCascade(deletedInventoriesIds, tx);
+        // await softDeleteSalesOnCascade(deletedInventoriesIds, tx);
       }
 
       res.status(200).json({message: "La libreria ha sido eliminada con exito."})
